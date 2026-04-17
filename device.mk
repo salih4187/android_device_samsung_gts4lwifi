18d2510e681694b65cd52fefe9fd979af02ae19e
Salih Çağlar <salihcaglar4187@gmail.com>
2026-04-17 16:24:19 +0300

    gts4lwifi: Build Keymaster HAL

--- device.mk
+++ device.mk
@@ -105,6 +105,12 @@ PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
     android.hardware.gatekeeper@1.0-service
 
+# Keymaster HAL
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@3.0-impl \
+    android.hardware.keymaster@3.0-service \
+    keystore.msm8998
+
 # QCOM
 PRODUCT_PACKAGES += \
     libjson \
