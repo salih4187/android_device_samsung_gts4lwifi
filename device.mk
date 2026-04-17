872c4ed24941c38c29636f85c4e8222ae5eab701
Salih Çağlar <salihcaglar4187@gmail.com>
2026-04-17 16:28:44 +0300

    gts4lwifi: Build Lights HAL

--- device.mk
+++ device.mk
@@ -111,6 +111,11 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-service \
     keystore.msm8998
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-impl \
+    android.hardware.light@2.0-service
+
 # QCOM
 PRODUCT_PACKAGES += \
     libjson \
